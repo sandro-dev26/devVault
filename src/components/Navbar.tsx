d312f143374ece6d6ba6cff353e447e732267975
sandro-dev26 <sadrokakhidze@gmail.com>
2026-07-21 21:34:50 +0400

    Style navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,14 +3,14 @@ import { NavLink } from "react-router-dom";
 function Navbar() {
   return (
     <>
-      <div className="flex flex-row gap-2 bg-slate-50 m-4 mb-12 p-2 justify-center items-center rounded-full w-60 h-12 justify-self-center transition-all duration-200 hover:bg-slate-200">
+      <div className="flex flex-row gap-2 bg-slate-800 m-4 mb-12 p-2 justify-center items-center rounded-full w-34 h-12 justify-self-center transition-all duration-200 hover:bg-slate-700 hover:scale-110">
         <NavLink
           to="/"
           end
           className={({ isActive }) =>
             isActive
-              ? "text-slate-50 transition-all duration-200 bg-slate-800 p-2 rounded-full"
-              : "text-slate-950 transition-all duration-200 p-2"
+              ? "text-neutral-50 bg-slate-900 transition-all duration-200 hover:bg-slate-800 hover:scale-110 p-2 rounded-full"
+              : "text-neutral-50 p-2 transition-all duration-200 hover:text-neutral-300 active:scale-90"
           }
         >
           Home
@@ -19,8 +19,8 @@ function Navbar() {
           to="/about"
           className={({ isActive }) =>
             isActive
-              ? "text-slate-50 transition-all duration-200 bg-slate-800 p-2 rounded-full"
-              : "text-slate-950 transition-all duration-200 p-2"
+              ? "text-neutral-50 bg-slate-900 transition-all duration-200 hover:bg-slate-800 hover:scale-110 p-2 rounded-full"
+              : "text-neutral-50 p-2 transition-all duration-200 hover:text-neutral-300 active:scale-90"
           }
         >
           About
