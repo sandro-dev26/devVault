032c3337621fd83ae2f4cdbbf4f5de4283297604
sandro-dev26 <sadrokakhidze@gmail.com>
2026-07-21 23:17:18 +0400

    Update about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,11 @@
 function About() {
   return (
     <>
-      <h1>About This Site</h1>
+      <h1 className="text-2xl">About This Site</h1>
+      <p className="text-sm">
+        This is a personal, non-commercial project built for learning,
+        showcasing data and insights on different coding technologies.
+      </p>
     </>
   );
 }
