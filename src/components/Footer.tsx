a106f9fd3203adea034e6cac7c55eb0b395ce663
sandro-dev26 <sadrokakhidze@gmail.com>
2026-07-21 23:21:50 +0400

    Fix footer alignment

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,11 @@
-function Foooter() {
+function Footer() {
   return (
-    <footer className="flex justify-center w-full">
-      <p className="flex justify-self-center text-neutral-400 text-sm font-light text-center w-[60%] mt-4">
+    <footer className="w-full mt-4">
+      <p className="w-[60%] mx-auto text-center text-neutral-400 text-sm font-light">
         This site is for learning purposes only. Data may be inaccurate.
       </p>
     </footer>
   );
 }
 
-export default Foooter;
+export default Footer;
